bb5cf38baf9846b7fd13df53d82fd8cccf3586ad
Andrew Tan <andrewwytan@gmail.com>
2022-07-11 23:57:27 +0800

    completed removeFromArray, sumAll ongoing.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+function removeFromArray (arrayInput,...args) {
+    newArray = [];
+    // use forEach to go through each element of the array
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    arrayInput.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    // return the new array
+    return newArray;
+} 
 
-};
+// alternative to .includes 
+// let check = false;
+// for(let i = 0; i < args.length; i++) {
+//     if(args[i] == item)
+//         check = true;
+// return check;
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+let j = 0;
 
+function sumAll (num1, num2) {
+    if (num2 > num1) {
+        for (let i = num1; i <= num2; i++) {
+            j += i
+            // console.log(`num1: ${num1}`);
+            // console.log(`num2: ${num2}`);
+            // console.log(`i: ${i}`);
+            // console.log(`j: ${j}`);
+        }
+    } else if (num1 > num2)  {
+        for (let i = num2; i <= num1; i++) {
+            j += i
+            // console.log(`num1: ${num1}`);
+            // console.log(`num2: ${num2}`);
+            // console.log(`i: ${i}`);
+            // console.log(`j: ${j}`);        
+        }
+    }
+    return j;
 };
 
 // Do not edit below this line
