58410588f0b20a1f28b2ce09d455ce8980440e24
Andrew Tan <andrewwytan@gmail.com>
2022-07-11 16:58:48 +0800

    completed js exercises up to 03.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+function repeatString(string, num) {
+    // string = 'hey'// prompt("Enter the word");
+    // num = 3 //parseInt(prompt("Enter the amount of times to repeat"));
+    if (num < 0) {
+        return "ERROR"
+    } else {
+    return string.repeat(num);
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+function reverseString(string) {
+    newString = string.split("").reverse().join("");
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
