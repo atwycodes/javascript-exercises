ccbb3d4962076e123918072a3a8ee798750dadb8
Andrew Tan <andrewwytan@gmail.com>
2022-10-24 00:33:06 +0800

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
 const add = function() {
-	
+	console.log(arguments);
+  let totalAdd = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    totalAdd += arguments[i];
+  }
+  return totalAdd;
 };
 
 const subtract = function() {
-	
+	console.log(arguments);
+  let totalSubtract = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    totalSubtract -= arguments[i];
+  }
+  return totalSubtract;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let sumArray = args[0]; //rest parameter is already an array. to call the multi-dimensional array, need to call the first index of rest parameter.
+  console.log(sumArray);
+  let totalSum = sumArray.reduce((first, last) => first + last, 0);
+    return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let multiplyArray = args[0]; //rest parameter is already an array. to call the multi-dimensional array, need to call the first index of rest parameter.
+  console.log(multiplyArray);
+  let totalMultiply = multiplyArray.reduce((first, last) => first * last);
+    return totalMultiply
 };
 
-const power = function() {
-	
+const power = function(...args) {
+	let totalPower = args.reduce((first, last) => first ** last);
+    return totalPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let arrayFactorial = [];
+  for (let i = 1; i <= a; i++) {
+    arrayFactorial.push(i);
+    console.log(arrayFactorial);
+  }
+  const totalFactorial = arrayFactorial.reduce((first,last) => first * last,1);
+  return totalFactorial;
 };
 
 // Do not edit below this line
