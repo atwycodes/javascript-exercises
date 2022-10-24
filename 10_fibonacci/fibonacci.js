a7031a6b98073b3474d9a46b55b99d39896018c0
Andrew Tan <andrewwytan@gmail.com>
2022-10-25 00:16:50 +0800

    completed 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let n = parseInt(input);
+    
+    if (n <= 2 && n > 0) {
+        let result = 1;
+        return result;
+    } else if (n > 2) {
+        let fibonacciArray = [1,1]
+        for (let i = 2; i < n; i++) {
+            let newValue = fibonacciArray[i-2] + fibonacciArray[i-1]
+            // console.log(newValue);
+            fibonacciArray.push(newValue);
+        }
+    // console.log(fibonacciArray[n-1]);
+    return fibonacciArray[n-1];
+    } else {
+        return ("OOPS");
+    };
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = fibonacci;
