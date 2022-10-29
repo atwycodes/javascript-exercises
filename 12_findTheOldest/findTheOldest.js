3c0306e02feb3abfcdd96b7f23607cd05ea846fd
Andrew Tan <andrewwytan@gmail.com>
2022-10-29 22:55:31 +0800

    completed 11_findTheOldest using reduce() and ternary operator

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    const oldest = array.reduce((previous,current) => {
+        let previousAge = ("yearOfDeath" in previous) ? (previous.yearOfDeath - previous.yearOfBirth) : (currentYear - previous.yearOfBirth);        
+        let currentAge = ("yearOfDeath" in current) ? (current.yearOfDeath - current.yearOfBirth) : (currentYear - previous.yearOfBirth);
 
+        return (previousAge > currentAge) ? previous : current;
+    });
+    return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
